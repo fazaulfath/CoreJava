ea146465aa3c7ed04e88e916447798a1922d0e7f
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-06-25 19:32:54 +0530

    commit

--- corejava/src/com/tnsif/encapsulation/Encapsulation.java
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -2,12 +2,53 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum=10; //data members
-	String name;
-	int age;
+	//default access specifier
+	private int serialNum=10; //data members
+	private String name;
+	private int age;
+	// by using getter and setter, we can access private data members
 	
-	void show() //member function
+	
+//	void show() //member function
+//	{
+//		System.out.println("Serial number:" + serialNum + "\t Name:" + name+ "\t Age:" + age);
+//	}
+
+
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+
+
+	public int getSerialNum() //read the value
 	{
-		System.out.println("Serial number:" + serialNum + "\t Name:" + name+ "\t Age:" + age);
+		return serialNum;
+	}
+
+
+	public void setSerialNum(int serialNum) //set the value
+	{
+		this.serialNum = serialNum;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 }
--- corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -5,9 +5,13 @@ public class EncapsulationDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Encapsulation ob =  new Encapsulation(); //object creation
-		ob.name = "faza"; //calling variables
-		ob.age = 33;
-		ob.serialNum = 22;
-		ob.show();
+//		ob.name = "faza"; //calling variables
+//		ob.age = 33;
+//		ob.serialNum = 22;
+		
+		ob.setAge(22);
+		ob.setName("faza");
+		ob.setSerialNum(33);
+		System.out.println(ob);
 	}
 }
