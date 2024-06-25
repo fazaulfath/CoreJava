d894aa81ed94a336dea0fabff5f3473eaf68b619
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-06-25 18:50:02 +0530

    commit

---
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,13 @@
+//program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum=10; //data members
+	String name;
+	int age;
+	
+	void show() //member function
+	{
+		System.out.println("Serial number:" + serialNum + "\t Name:" + name+ "\t Age:" + age);
+	}
+}
---
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob =  new Encapsulation(); //object creation
+		ob.name = "faza"; //calling variables
+		ob.age = 33;
+		ob.serialNum = 22;
+		ob.show();
+	}
+}
