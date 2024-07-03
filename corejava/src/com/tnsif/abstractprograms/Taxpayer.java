514b3aa1078cb8a2d8250851d887fa41f64d7cfb
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-07-03 17:45:03 +0530

    abstract commit

---
+++ corejava/src/com/tnsif/abstractprograms/Taxpayer.java
@@ -0,0 +1,18 @@
+package com.tnsif.abstractprograms;
+// Non concrete class
+public  abstract class Taxpayer 
+{
+	protected double salary; // instance variable 
+	public abstract double  calTax(); // abstract method 
+	public void display() 
+	{
+		System.out.println("the tax payed is " + calTax());
+	
+	}
+	public Taxpayer(double salary) // parameterized constructor
+	{ 
+		super();
+		this.salary = salary;
+	}
+
+}
